fd66250ec8a628e194e217363a47cde6d8a2daab
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-07-03 15:18:39 -0500

    added app-root template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
+import { MaterialUiModule } from './material-ui/material-ui.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TaskTableComponent } from './task-table/task-table.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, TaskTableComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSliderModule,
+    MaterialUiModule,
     BrowserAnimationsModule,
   ],
   providers: [],
---
+++ src/app/material-ui/material-ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [],
+  exports: [MatToolbarModule, MatInputModule],
+})
+export class MaterialUiModule {}
