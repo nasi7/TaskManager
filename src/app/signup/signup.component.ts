af604eecdd24333cdb7496396d323c7252a5c416
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-07-04 14:57:25 -0500

    added signup template form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  hide = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleSubmit(signUpForm: NgForm) {
+    console.log(signUpForm);
   }
-
 }
