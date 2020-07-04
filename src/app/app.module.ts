9239e1a1d09bebc2c09fe23ddcb4a08a97040ae9
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-07-04 14:41:49 -0500

    added login form submission functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialUiModule } from './material-ui/material-ui.module';
+import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { TaskTableComponent } from './task-table/task-table.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, TaskTableComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TaskTableComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialUiModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +8,18 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   hide = true;
+  private username: string;
+  private password: string;
   constructor() {}
 
   ngOnInit(): void {}
+
+  handleSubmit(loginForm: NgForm) {
+    this.username = loginForm.controls.username.value;
+    this.password = loginForm.controls.password.value;
+    this.validateUser();
+  }
+
+  //TODO
+  validateUser() {}
 }
